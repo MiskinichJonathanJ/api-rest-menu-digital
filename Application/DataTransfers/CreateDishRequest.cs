cef268975ab2419625d419047701f1559df4314d
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-27 18:32:26 -0300

    feat: implementar dto para request  de creacion y response de dish

---
+++ Application/DataTransfers/CreateDishRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DataTransfers
+{
+    public class CreateDishRequest
+    {
+        public string DishName { get; set; }
+        public string DishDescription { get; set; }
+        public decimal DishPrice { get; set; }
+        public bool DishIsAvailable { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public int CategoryOrder { get; set; }
+    }
+}
---
+++ Application/DataTransfers/DishResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DataTransfers
+{
+    public class DishResponse
+    {
+        public string DishName { get; set; }
+        public string DishDescription { get; set; }
+        public decimal DishPrice { get; set; }
+        public bool DishIsAvailable { get; set; }
+        public CategoryResponse DishCategory { get; set; }
+
+    }
+}
