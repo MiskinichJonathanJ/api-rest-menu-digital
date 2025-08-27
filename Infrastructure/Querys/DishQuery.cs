91c84976418fbf74d1ee85a4df7ce210207ac12e
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-27 18:34:13 -0300

    feat: agregar patron CQRS

---
+++ Infrastructure/Querys/DishQuery.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Querys
+{
+    public class DishQuery : IDishQuery
+    {
+        public Task<ICollection<Dish>> GetAllDish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dish> GetDishById(int dishId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
