fff839efc09f0b7de1be1319e66c31b0ac402c2f
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-27 13:34:40 -0300

    fix:  pasar las  entidades al dominio

--- MenuDigitalRestaurante/Entities/deliveryType.cs
+++
@@ -1,9 +0,0 @@
-namespace MenuDigitalRestaurante.Entities
-{
-	public class DeliveryType
-	{
-		public int ID { get; set; }
-		public string Name { get; set; }
-		public  ICollection<Order> OrdersNav { get; set; }
-	}
-}
