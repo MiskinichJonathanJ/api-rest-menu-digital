ee1b19814719d65521eb441da308bd8c082edb0c
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-27 13:36:03 -0300

    feat: agregar los  dbset  para  las tablas en la base de datos

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -9,6 +9,9 @@ namespace Infrastructure.Persistence
 {
     public class AppDbContext : DbContext
     {
-
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
     }
 }
