6517a1332d54c730b373e7b4a156b5c35c363ba3
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-27 18:35:00 -0300

    feat:  agregar interfaces para intyeccion de dependencias

---
+++ Application/Interfaces/IDishServices.cs
@@ -0,0 +1,20 @@
+﻿using Application.DataTransfers;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IDishServices
+    {
+        Task<DishResponse> CreateDish(CreateDishRequest request);
+        Task DeleteDish(int id);
+        Task<ICollection<Dish>> GetAllDish();
+        Task<Dish> GetDishById(int id);
+        Task UpdateDish(int id, Dish dish);
+
+    }
+}
---
+++ Application/UseCase/Dish/DishServices.cs
@@ -0,0 +1,53 @@
+﻿using Application.DataTransfers;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Domain.Entities;
+
+namespace Application.UseCase.DishUse
+{
+    public class DishServices : IDishServices
+    {
+        public readonly IDishCommand _command;
+        public readonly IDishQuery _query;
+        public readonly IDishMapper _mapper;
+
+        public DishServices(IDishCommand command, IDishQuery query, IDishMapper mapper)
+        {
+            _command = command;
+            _query = query;
+            _mapper = mapper;
+        }
+
+        public async Task<DishResponse> CreateDish(CreateDishRequest request)
+        {
+            var dish = _mapper.ToEntity(request);
+            await _command.CreateDish(dish);
+            return _mapper.ToResponse(dish);  
+        } 
+
+        public Task DeleteDish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Dish>> GetAllDish()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dish> GetDishById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDish(int id, Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
