9d0d59e9d3c26ce1676ce34b5fbfbec9a45b5bee
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-31 11:04:05 -0300

    cam,biar estructura de carpetas  en data  transfer

--- Application/DataTransfers/Response/DishResponse.cs
+++ Application/DataTransfers/Response/DishResponse.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.DataTransfers
+namespace Application.DataTransfers.Response
 {
     public class DishResponse
     {
