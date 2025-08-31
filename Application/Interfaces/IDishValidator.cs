74b6001e60e6ec731162f4a8f4b3c195b5569f11
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-08-31 15:31:28 -0300

    feat:  agregar estructura para validaciones

---
+++ Application/Interfaces/IDishValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.DataTransfers.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    internal interface IDishValidator
+    {
+        void ValidateCreate(DishRequest request);
+        void ValidateUpdate();
+    }
+}
---
+++ Application/Validations/DishValidator.cs
@@ -0,0 +1,32 @@
+﻿using Application.DataTransfers.Request;
+using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validations
+{
+    internal class DishValidator : IDishValidator
+    {
+        private readonly IDishQuery _query;
+
+        public DishValidator(IDishQuery query)
+        {
+            _query = query;
+        }
+
+        public async void ValidateCreate(DishRequest request)
+        {
+            var category = await _query.GetCategoryById(request.CategoryId);
+            if (category == null)
+                throw new Exception("La categoria no existe");
+        }
+
+        public void ValidateUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
